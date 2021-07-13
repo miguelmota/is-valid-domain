7a5c29efcfaf822888fee009736ef0148efe2ca9
Miguel Mota <hello@miguelmota.com>
2021-07-13 00:51:02 -0700

    Update example

--- example/dist/bundle.js
+++ example/dist/bundle.js
@@ -1700,6 +1700,7 @@ var isValidDomain = require('../')
 
 var domain = document.querySelector('#domain')
 var subdomain = document.querySelector('#subdomain')
+var topLevel = document.querySelector('#topLevel')
 var wildcard = document.querySelector('#wildcard')
 var unicode = document.querySelector('#unicode')
 var submit = document.querySelector('#submit')
@@ -1708,6 +1709,7 @@ var output = document.querySelector('#output')
 submit.addEventListener('click', update)
 domain.addEventListener('input', update)
 subdomain.addEventListener('change', update)
+topLevel.addEventListener('change', update)
 wildcard.addEventListener('change', update)
 unicode.addEventListener('change', update)
 
@@ -1717,6 +1719,7 @@ function update(event) {
   if (event) event.preventDefault()
   output.innerHTML = String(isValidDomain(domain.value, {
     subdomain: subdomain.checked,
+    topLevel: topLevel.checked,
     wildcard: wildcard.checked,
     allowUnicode: unicode.checked,
   }))
--- example/main.js
+++ example/main.js
@@ -2,6 +2,7 @@ var isValidDomain = require('../')
 
 var domain = document.querySelector('#domain')
 var subdomain = document.querySelector('#subdomain')
+var topLevel = document.querySelector('#topLevel')
 var wildcard = document.querySelector('#wildcard')
 var unicode = document.querySelector('#unicode')
 var submit = document.querySelector('#submit')
@@ -10,6 +11,7 @@ var output = document.querySelector('#output')
 submit.addEventListener('click', update)
 domain.addEventListener('input', update)
 subdomain.addEventListener('change', update)
+topLevel.addEventListener('change', update)
 wildcard.addEventListener('change', update)
 unicode.addEventListener('change', update)
 
@@ -19,6 +21,7 @@ function update(event) {
   if (event) event.preventDefault()
   output.innerHTML = String(isValidDomain(domain.value, {
     subdomain: subdomain.checked,
+    topLevel: topLevel.checked,
     wildcard: wildcard.checked,
     allowUnicode: unicode.checked,
   }))
