3196159451deb216b47f138affb968b93db490d4
Miguel Mota <hello@miguelmota.com>
2021-07-13 21:42:32 -0700

    Update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,3 @@
-declare function isValidDomain(text: string, opts?: { subdomain?: boolean; wildcard?: boolean, allowUnicode?: boolean }): boolean;
+declare function isValidDomain(text: string, opts?: { subdomain?: boolean; wildcard?: boolean, allowUnicode?: boolean, topLevel?: boolean }): boolean;
 
 export = isValidDomain;
